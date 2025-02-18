a2d4059f62df78ddf5209a9d33f0b4be45859d75
UP240041 <up240041@alumnos.upa.edu.mx>
2025-02-18 09:10:31 -0600

    avance2_dia3

--- ejercicios_dia3.py
+++ ejercicios_dia3.py
@@ -39,14 +39,57 @@ print("El área del círculo es: ", area) #Se imprime el área
 
 ##Ejercicio 6
 ##Pendiente de la recta "y=2x-2"
-pendiente= 2 #Se define la pendiente
+pendiente1= 2 #Se define la pendiente
 intersecciónY= -2 #Se define la intersección con el eje Y
-intersecciónX= intersecciónY/pendiente #Se calcula la intersección con el eje X
-print("La pendiente de la recta es:\n", pendiente, "\nLa intersección con el eje X es:\n", intersecciónX, "\nLa intersección con el eje Y es:\n", intersecciónY)
+intersecciónX= intersecciónY/pendiente1 #Se calcula la intersección con el eje X
+print("La pendiente de la recta es:\n", pendiente1, "\nLa intersección con el eje X es:\n", intersecciónX, "\nLa intersección con el eje Y es:\n", intersecciónY)
 
 ##Ejercicio 7
 ##Pendiente dos puntos
 punto1= (2,2) #Se define el punto 1
 punto2= (6,10) #Se define el punto 2
 x1= punto1[0] #Se obtiene la coordenada x del punto 1
-print(x1)
+y1= punto1[1] #Se obtiene la coordenada y del punto 1
+x2= punto2[0] #Se obtiene la coordenada x del punto 2
+y2= punto2[1] #Se obtiene la coordenada y del punto 2
+pendiente2= (y2-y1)/(x2-x1) #Se calcula la pendiente
+distancia= ((x2-x1)**2+(y2-y1)**2)**0.5 #Se calcula la distancia entre los dos puntos
+print("La pendiente de los puntos es:", pendiente2, "\nLa distancia entre los puntos es:", distancia)
+
+##Ejercicio 8
+##Comparar pendientes
+pendiente1<pendiente2 #Se compara si la pendiente 1 es menor que la pendiente 2
+if True:
+    print("La pendiente 1 es menor que la pendiente 2")
+else:
+    print("La pendiente 1 no es menor que la pendiente 2")
+
+##Ejercicio 9
+##Comparar longitudes de palabras
+palabra1= "python" #Se define la palabra 1
+palabra2= "dragon" #Se define la palabra 2
+longitud1= len(palabra1) #Se obtiene la longitud de la palabra 1
+longitud2= len(palabra2) #Se obtiene la longitud de la palabra 2
+longitud1<longitud2 #Se compara si la longitud de la palabra 1 es menor que la longitud de la palabra 2
+if True:
+    print("La longitud de la palabra 1 es menor que la longitud de la palabra 2")
+else:
+    print("La longitud de la palabra 1 no es menor que la longitud de la palabra 2")
+
+##Ejercicio 10
+##Esta en?
+
+
+
+
+
+
+
+
+
+##Ejercicio 
+##Converciones
+palabra="python" #Se define la palabra
+longitud= len(palabra) #Se obtiene la longitud de la palabra
+pirnt("on" in "python" and "on" in "dragon") #Se imprime si "on" está en "python" y en "dragon"
+
