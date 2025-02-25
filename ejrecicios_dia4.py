ceb64ef216c1931afa7a061e305125f1a9b8cc5c
UP240041 <#up240041@alumnos.upa.edu.mx>
2025-02-24 19:01:36 -0600

    avance1_dia4

---
+++ ejrecicios_dia4.py
@@ -0,0 +1,36 @@
+##Ejercicios dia 4
+
+##Ejercicio 1
+##Concatenar1
+plabra1="Thirty"
+palabra2="Days"
+palabra3="Of"
+palabra4="Python"
+espacio=" "
+concatenar=plabra1+espacio+palabra2+espacio+palabra3+espacio+palabra4
+print(concatenar)
+
+##Ejercicio 2
+##Concatenar2
+palabra1="Coding"
+palabra2="For"
+palabra3="All"
+espacio=" "
+concatenar=palabra1+espacio+palabra2+espacio+palabra3
+print(concatenar)
+
+##Ejercicio 3
+##Decalración de variable
+company="Coding For All"
+
+##Ejercicio 4
+##Imprimir la variable company
+print(company)
+
+##Ejercicio 5
+##Imprimir el tamaño de la variable company
+print(len(company))
+
+##Ejercicio 6
+##Todas las letras en mayusculas
+print(company.upper())
