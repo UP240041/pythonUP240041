2315da6b0ae3cb97665a40edd802dea3804db51c
UP240224 <up240224@alumnos.upa.edu.mx>
2025-02-25 09:37:29 -0600

    avance3_dia4

--- ejrecicios_dia4.py
+++ ejrecicios_dia4.py
@@ -74,5 +74,111 @@ print(EP.replace("Python","All"))
 CFA="Coding For All"
 print(CFA.split(" "))
 
+##Ejercicio 14
+##Separar2
+texto="Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print(texto.split(","))
 
+##Ejercicio 15
+##Index
+CFA="Coding For All"
+print(CFA[0])
 
+##Ejercicio 16
+##Rindex
+print(CFA.rindex("l"))
+
+##Ejercicio 17
+##Imprimir index
+CFA="Coding For All"
+print(CFA[10])
+
+##Ejercicio 18
+##Acronimo1
+PFE="Python For Everyone"
+primero=PFE.index("P")
+segundo=PFE.index("F")
+tercero=PFE.index("E")
+acronimo=PFE[primero]+PFE[segundo]+PFE[tercero]
+print(acronimo)
+
+##Ejercicio 19
+##Acronimo2
+CFA="Coding For All"
+primero=CFA.index("C")
+segundo=CFA.index("F")
+tercero=CFA.index("A")
+acronimo=CFA[primero]+CFA[segundo]+CFA[tercero]
+print(acronimo)
+
+##Ejercicio 20
+##index
+CFA="Coding For All"
+print(CFA.index("C")) 
+
+##Ejercicio 21
+##index2
+CFA="Coding For All"
+print(CFA.index("F"))
+
+##Ejercicio 22
+##rfind
+texto="Coding For All People"
+print(texto.rfind("l"))
+
+##Ejercicio 23
+##Oración index
+texto="You cannot end a sentence with because because because is a conjunction"
+print(texto.index("because"))
+
+##Ejercicio 24
+##Oración rindex
+texto="You cannot end a sentence with because because because is a conjunction"
+print(texto.rindex("because"))
+
+##Ejercicio 25
+##Oración Cortar
+texto="You cannot end a sentence with because because because is a conjunction"
+fin=texto.index("because")-1
+inicio=texto.rindex("e")+1
+textoCortado=texto[0:fin]+texto[inicio:]
+print(textoCortado)
+
+##Ejercicio 26
+##Oración index2
+texto="You cannot end a sentence with because because because is a conjunction"
+print(texto.index("because"))
+
+##Ejercicio 27
+##Oración Cortar2
+texto="You cannot end a sentence with because because because is a conjunction"
+fin=texto.index("because")-1
+inicio=texto.rindex("e")+1
+textoCortado=texto[0:fin]+texto[inicio:]
+print(textoCortado)
+
+##Ejercicio 28
+##satrtswith
+CFA="Coding For All"
+inicio="Coding"
+print(CFA.startswith(inicio))
+
+##Ejercicio 29
+##endswith
+CFA="Coding For All"
+fin="coding"
+print(CFA.endswith(fin))
+
+##Ejercicio 30
+##Quitar espacios
+texto='   Coding For All      '
+inicio=texto.index("C")
+fin=texto.rindex("l")+1
+textosinespacios=texto[inicio:fin]	
+print(textosinespacios)
+
+##Ejercicio 31
+##isidentifier
+texto1="30DaysOfPython"
+texto2="thirty_days_of_python"
+print(texto1.isidentifier())
